46ccb55bcd0d62b75b853182d1988dc144159c6d
CloudJumper123 <107224964+CloudJumper123@users.noreply.github.com>
2022-06-24 16:10:50 -0700

    Update surf_detection_trial.m

--- surf_detection_trial.m
+++ surf_detection_trial.m
@@ -1,46 +1,66 @@
-I = imread('image1.png');
+I = imread('test3.png');
+I = imgaussfilt(I);
 
-%Since size(I,2)/2 = 215.5, I have set the value '215' directly for x
-%x = [top bottom]
-x1 = [50 50];
-y1 = [0 size(I,1)];
+%setting the vertical sample line
+x1 = [50 50];           %x1 = [length from top left    length from bottom left]
+y1 = [0 size(I,1)];    %size(I, 1) = height of image/length of sample line
 
-c = improfile(I,x1,y1);
+
+intensities = improfile(I,x1,y1);  %find intensity pixels going down the sample line
+intensities = intensities(2:end);            %removes the invalid value in beginning
+
 figure
-subplot(2,1,1)
+subplot(2,1,1)      %subplot(number of rows, number of columns, figure#)
 imshow(I);
 hold on
-plot(x1,y1,'r')  %plot sample line
+plot(x1,y1,'r')    %plot sample line
 
 
+%plot intensity graph under picture with sample line
 subplot(2,1,2)
-plot(c(:,1,1),'r')
+plot(intensities(:,1,1),'r')   
+xlabel('Pixel # going down the line')
+ylabel('Pixel Intensity')
 
-x2 = linspace(0, size(I,1), size(I,1) +1);
-y2 = c(:,1,1);
-
+%Find all local max and print intensity graph with them labelled
+x2 = linspace(1, size(I,1), size(I,1));
+y2 = intensities;
 TF = islocalmax(y2);
 figure(); plot(x2,y2, x2(TF),y2(TF),'r*');
 
-y_localmax_list = y2(TF);
 
-two_peaks = maxk(y_localmax_list, 2);
+two_peaks = maxk(y2(TF), 2);     %finding two largest local max from all local max: y2(TF)
 
-peak1_y = two_peaks(1);
-peak2_y = two_peaks(2);
+peak1_y = two_peaks(1);          %first local max
+peak2_y = two_peaks(2);          %second local max
 
-id1 = find(y2 == peak1_y);
-peak1_x = x2(id1);
+id1 = find(y2 == peak1_y);       %find x for first local max
+peak1_x = x2(id1);   
 
-id2 = find(y2 == peak2_y);
+id2 = find(y2 == peak2_y);       %find y for second local max
 peak2_x = x2(id2);
 
-z1 = peak1_x + ((peak2_x - peak1_x)/2);
-disp(round(z1))
+% z1 = peak1_x + ((peak2_x - peak1_x)/2);
+% disp(round(z1))
 
 
+%Find derivative of intensity graph and show it
+dy=diff(y2)./diff(x2);
+dy = dy(:,1);
+dy = [0; dy];
+figure(); plot(x2(1:end),dy);
 
 
+interval1 = [peak1_x-15 peak1_x];   %interval to search for start of lay 1
+
+%Search for maximum 
+C = zeros(1,numel(interval1)-1);
+for k=1:numel(interval1)-1
+C(k)=max(dy(interval1(k):interval1(k+1)));
+end
+
+id3 = find(dy(:,1) == C(:,1));       
+lay1_start = x2(id3);
 
 
-
+disp(['Start of layer 1 is pixel ', num2str(lay1_start)])
