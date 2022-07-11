3c1b31a263d118fc538e9b9dfe6f885435b88e68
CloudJumper123 <107224964+CloudJumper123@users.noreply.github.com>
2022-07-11 14:16:07 -0700

    Update surf_detection_trial.m

--- surf_detection_trial.m
+++ surf_detection_trial.m
@@ -1,5 +1,6 @@
-function first_layer = surf_detection_trial(pic, index)
+function [first_layer, second_layer] = surf_detection_trial(pic, index)
     I = imread(pic);
+    I = imgaussfilt(I);
     I = imadjust(I, [0.15,1]);
     imbw = imbinarize(I);
     im_opened = bwareaopen(imbw, 800,8); %600 default
@@ -9,11 +10,10 @@ function first_layer = surf_detection_trial(pic, index)
 
     %setting the vertical sample line
     x1 = [index index];           %x1 = [length from top left    length from bottom left]
-    y1 = [0 size(I, 1)];    %size(I, 1) = height of image/length of sample line
+    y1 = [1 size(I, 1)];    %size(I, 1) = height of image/length of sample line
     
     
     intensities = improfile(I,x1,y1);  %find intensity pixels going down the sample line
-    intensities = intensities(2:end);            %removes the invalid value in beginning
     
     % figure
     % subplot(2,1,1)      %subplot(number of rows, number of columns, figure#)
@@ -35,22 +35,32 @@ function first_layer = surf_detection_trial(pic, index)
     % figure(); plot(x2,y2, x2(TF),y2(TF),'r*');
     
     
-    two_peaks = maxk(y2(TF), 12);     %finding two largest local max from all local max: y2(TF)
+    two_peaks = maxk(y2(TF), length(y2(TF)));     %sort all local max in decreasing order.
 
     peak1_y = two_peaks(1);          %first local max
-    % peak2_y = two_peaks(2); 
-    % peak3_y = two_peaks(3);           %third peak
-    
     id1 = find(y2 == peak1_y);       %find x for first local max
     peak1_x = x2(id1(1));   
+
     
-    for i=2:12
+    for i=2:length(y2(TF))
         peaki_y = two_peaks(i);
         idi = find(y2 == peaki_y);       %find x for first local max
         peaki_x = x2(idi(1));
         if abs(peaki_x - peak1_x) > 30
             break
         end
+    end
+    
+%     figure(); plot(x2,y2)
+%     hold on
+%     plot(peak1_x, peak1_y, 'r.', 'MarkerSize', 15)
+%     hold on
+%     plot(peaki_x, peaki_y, 'b.', 'MarkerSize', 15)
+    
+    if peak1_x > peaki_x
+        c = peak1_x;
+        peak1_x = peaki_x;
+        peaki_x = c;
     end
     
     
@@ -64,10 +74,7 @@ function first_layer = surf_detection_trial(pic, index)
     %     peak2_x = peak3_x;
     % end
     
-    if peak1_x > peaki_x
-        peak1_x = peaki_x;
-    end
-    
+%     
     %Find derivative of intensity graph
     dy=diff(y2)./diff(x2);
     
@@ -75,24 +82,39 @@ function first_layer = surf_detection_trial(pic, index)
     
     %figure(); plot(x2(1:249),dy);    
     
-    interval1 = [peak1_x-15 peak1_x];   %interval to search for start of lay 1
-    
-    %Search for maximum slope in the interval above
-    max_slope = zeros(1,numel(interval1)-1);
+    interval1 = [peak1_x-25 peak1_x];   %interval to search for start of lay 1
+    interval2 = [peaki_x-25 peaki_x];
+
+    %Search for maximum slope in the interval 1
+    max_slope1 = zeros(1,numel(interval1)-1);
     for k=1:numel(interval1)-1
-    max_slope(k)=max(dy(interval1(k):interval1 ...
+    max_slope1(k)=max(dy(interval1(k):interval1 ...
         (k+1)));
     end
     
-    id3 = find(dy((peak1_x-15):peak1_x) == max_slope);
+    id3 = find(dy((peak1_x-25):peak1_x) == max_slope1);
     lay1_start = x2(id3);
     
     if length(lay1_start) ~= 1
         lay1_start = lay1_start(:,2);
     end
+
+    first_layer  = lay1_start + peak1_x - 26;
    
-    first_layer  = lay1_start + peak1_x - 16;
+    %Search for maximum slope in the interval 2 
+    max_slope2 = zeros(1,numel(interval2)-1);
+    for k=1:numel(interval2)-1
+    max_slope2(k)=max(dy(interval2(k):interval2 ...
+        (k+1)));
+    end
+    
+    id4 = find(dy((peaki_x-25):peaki_x) == max_slope2);
+    lay2_start = x2(id4);
+    
+    if length(lay2_start) ~= 1
+        lay2_start = lay2_start(:,2);
+    end
+    
+    second_layer  = lay2_start + peaki_x - 26;
     
 %     disp(['Start of layer ' , num2str(index), ' is pixel ', num2str(lay1_start)])
-
-
