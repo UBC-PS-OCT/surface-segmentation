74cd55d43bf48a8aff12bbafec5231ff911d1a02
CloudJumper123 <107224964+CloudJumper123@users.noreply.github.com>
2022-07-19 14:14:40 -0700

    Update Layer_Segmentation_Main.m

--- Layer_Segmentation_Main.m
+++ Layer_Segmentation_Main.m
@@ -1,11 +1,12 @@
 %%%  Running this program requires Xin's surface_smooth.m function, layer_start.m function, and layer_search.m function    %%%
 
 image = 'image8.png';
+im = imread(image)
 
 %find the beggining of first and second layers
-surf1 = zeros(1,250);
-surf2 = zeros(1,250);
-for i = 1:250
+surf1 = zeros(1, size(im, 1));
+surf2 = zeros(1, size(im, 1));
+for i = 1:size(im, 1)
     [point1, point2] = layer_start(image, i);
     surf1(i) = point1;
     surf2(i) = point2;
@@ -15,7 +16,7 @@ end
 surf1_smooth = surface_smooth(surf1, 6);
 surf2_smooth = surface_smooth(surf2, 60);
 
-j = linspace(1,250,250);
+j = linspace(1, size(im, 1), size(im, 1));
 
 %plot original picture with the two layers
 figure('Name', image); imshow(image);
