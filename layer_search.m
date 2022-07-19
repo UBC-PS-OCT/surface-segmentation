5c19144db7015f150e8d4960638e92bbe9cd15e0
CloudJumper123 <107224964+CloudJumper123@users.noreply.github.com>
2022-07-18 23:15:54 -0700

    Update layer_search.m

--- layer_search.m
+++ layer_search.m
@@ -1,11 +1,9 @@
 function [starting_pixel] = layer_search(interval, interval_size, dy, peak_x,x)
-    max_slope = max(dy(interval(1):interval(2)));
+    max_slope = max(dy(interval(1):interval(2))); 
+    x_values = find(dy((peak_x-interval_size):peak_x) == max_slope);
     
-    id3 = find(dy((peak_x-interval_size):peak_x) == max_slope);
-    lay_start = x(id3);
-    
-    if length(lay_start) ~= 1
-        lay_start = lay_start(:,2);
+    if length(x_values) ~= 1
+        x_values = x_values(:,2);
     end
 
-    starting_pixel  = lay_start + peak_x - interval_size - 1;
+    starting_pixel  = x_values + peak_x - interval_size - 1;
