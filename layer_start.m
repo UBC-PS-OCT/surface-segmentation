5dbef46826255a2a9dfdaea5c9dde7fd611b3af0
CloudJumper123 <107224964+CloudJumper123@users.noreply.github.com>
2022-08-08 16:54:22 -0700

    Update layer_start.m

--- layer_start.m
+++ layer_start.m
@@ -1,3 +1,80 @@
+function [first_layer, second_layer] = layer_start(I, index)
+
+%setting the vertical sample line (slice)
+%index = slice position
+slice_height = [1 size(I, 1)];    %size(I, 1) = height of image/length of slice
+intensities = improfile(I,[index index],slice_height);  %find intensity of pixels going down the slice
+
+%if the slice is pure black, we exit function and return
+%zero for both layers, which represent no layers
+if intensities == zeros (size(I, 1), 1)
+    first_layer = NaN;
+    second_layer = NaN;
+    return
+end
+
+%Find all local max and print intensity graph with them labelled
+x = linspace(1, size(I,2), size(I,2));
+y = intensities;
+TF = islocalmax(y);
+
+%Find derivative of intensity graph
+dy = diff(y)./diff(x);
+dy = dy(:,1);  %keep first column since all of them are identical
+%figure(); plot(x(1:249),dy);  
+
+%finds and sorts all local max in decreasing order
+peaks = maxk(y(TF), length(y(TF)));     
+
+%Find first/highest local max and its x value           
+id1 = find(y == peaks(1));      
+peak1_x = id1(1);   
+
+%if there is only one peak, find first layer and exit function
+if length(y(TF)) == 1
+    interval_size = 10;
+    interval1 = [peak1_x peak1_x+interval_size];  
+    if interval1(1)<0
+        interval1(1)=0;
+    end
+    
+    if interval1(2) > length(dy)
+        interval1(2) = length(dy);
+    end
+    first_layer = layer_search(interval1, dy, peak1_x, interval_size, 1);
+    second_layer = NaN;
+    return
+end
+
+%Find the second highest local max whose x-value 
+%is 30px away from the first
+for i=2:length(y(TF))
+    peaki_y = peaks(i);
+    idi = find(y == peaki_y);
+    if length(idi) > 1
+        for j=1:length(idi)
+            peaki_x = idi(j);
+            if abs(peaki_x - peak1_x) > 30
+                break
+            end
+        end
+    else
+        peaki_x = idi;
+    end
+    if abs(peaki_x - peak1_x) > 30 
+        break
+    end
+end
+
+% We want peak1_x to represents the first layer. Hence, peak1_x and 
+%peaki_x needs to be swaped if peak1_x > peaki_x.
+if peak1_x > peaki_x
+    c = peak1_x;
+    peak1_x = peaki_x;
+    peaki_x = c;
+end
+ 
+% %define intervals to search for the beginning of lay1 and lay2
 interval_size = 10;
 interval1 = [peak1_x peak1_x + interval_size];  
 interval2 = [peaki_x-interval_size peaki_x ];
@@ -55,7 +132,7 @@ else
     first_layer = layer_search(interval1,dy,peak1_x,interval_size, 1);
     second_layer = NaN;
 end
-
+end
 
 
 
@@ -72,7 +149,7 @@ function [starting_pixel] = layer_search(interval, dy, peak_x, interval_size, la
     end
     
     if layer_num == 1
-        starting_pixel  = peak_x + x_values - 1;
+        starting_pixel  = peak_x + x_values - 1 + 10;
     else
         starting_pixel = x_values + peak_x - interval_size - 1;
     end
